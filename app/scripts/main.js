9aa890c8effceccd45c2ee011a1ee08291c93d92
Wibe Harteveld <flyer090@gmail.com>
2014-03-10 16:42:20 +0100

    Script based on div instead of data-attr injection

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,15 +2,7 @@
 jQuery(document).ready(function($) {
 	'use strict';
 
-	var i, length,
-	table = $('.table.reflow'),
-	tbodyTrs = table.find('> tbody > tr'),
-	tdWithColspan = table.find('td[colspan]'),
-	curTdWithColspan,
-	colspanNum,
-	tdDataAttributes,
-
-	getPolisNames = function (table) {
+	var getTheadThContent = function (table) {
 		var i, length, th,
 		tableTheadThs = table.find('> thead > tr > th'),
 		polisName = '',
@@ -25,7 +17,7 @@ jQuery(document).ready(function($) {
 
 			// if th is not empty, push polis name in polisnames array
 			if ( th.childNodes.length !== 0 ) {
-				polisName = $.trim( th.childNodes[0].nodeValue );
+				polisName = $.trim( th.innerHTML );
 				polisNames.push( polisName );
 			}
 		}
@@ -33,11 +25,15 @@ jQuery(document).ready(function($) {
 		return polisNames;
 	},
 
-	// Set each td with a data-attribute polis to set polis names in css
-	setDataAttributes = function (tbodyTrs, dataAttributeArray) {
+	// Set each td content
+	toggleTdSpan = function ( tbodyTrs, nodeContent ) {
 		var i, j, length,
 		trChild,
-		tdNum;
+		tdNum,
+		thInjected;
+
+		// Set optional nodeContent
+		nodeContent = (typeof nodeContent === 'undefined') ? false : nodeContent;
 
 		// Iterate over each tr
 		for (i = 0, length = tbodyTrs.length; i < length; i++) {
@@ -49,16 +45,69 @@ jQuery(document).ready(function($) {
 
 				// Only td's go through
 				if ( trChild.tagName !== 'TH' ) {
-					console.log(tdNum);
-					$(trChild).attr( 'data-polis', dataAttributeArray[ tdNum ] );
+					thInjected = $(trChild).find('.th-injected');
+
+					// Add or remove div in td (with thead th content)
+					if ( nodeContent && thInjected.length === 0 ) {
+						trChild.innerHTML = '<div aria-hidden="true" class="th-injected">' +
+							nodeContent[ tdNum ] + '</div>' + trChild.innerHTML;
+					} else if ( thInjected.length !== 0 ) {
+						trChild.removeChild( thInjected[0] );
+					}
+
 					tdNum += 1;
 				}
 			}
 		}
-	};
+	},
 
-	tdDataAttributes = getPolisNames( table );
-	setDataAttributes( tbodyTrs, tdDataAttributes );
+	i, length,
+	table = $('.table.reflow'),
+	tbodyTrs = table.find('> tbody > tr'),
+	tdWithColspan = table.find('td[colspan]'),
+	curTdWithColspan,
+	colspanNum,
+	theadThsContent = getTheadThContent( table ),
+	mediaQueryBreakpoint = '767px',
+	mediaQueryIsSet = false;
+
+	// Initiate table reflow when media query breakpoint is reached
+	if( Modernizr.mq('(max-width: ' + mediaQueryBreakpoint + ')') ) {
+		// Insert span tags with th content in each td
+		toggleTdSpan( tbodyTrs, theadThsContent );
+
+		mediaQueryIsSet = true;
+	}
+
+	// Add an event listener for checking browser resizing
+	$(window).on('resize', function() {
+
+		// Check if media query breakpoint is reached
+		if( Modernizr.mq('(max-width: ' + mediaQueryBreakpoint + ')') ) {
+
+			// Assuring that toggleTdSpan will be invoked only once
+			if ( !mediaQueryIsSet ) {
+				// Add div .th-injected for each td
+				toggleTdSpan( tbodyTrs, theadThsContent );
+
+				mediaQueryIsSet = true;
+			}
+		}
+		// Assuring that toggleTdSpan will be invoked only once
+		else if ( mediaQueryIsSet ) {
+
+			// Remove div .th-injected for each td
+			toggleTdSpan( tbodyTrs );
+
+			mediaQueryIsSet = false;
+		}
+	});
+
+
+
+
+
+
 
 	// Set for each td a data-polis attr
 	// for (i = 0, length = td.length; i < length; i++) {
